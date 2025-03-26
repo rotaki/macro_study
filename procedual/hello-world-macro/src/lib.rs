595f69fb8c0ec78184715981385f2ba63eebebbd
wattlebirdaz <wattlebirdaz@gmail.com>
2025-03-26 10:37:18 -0500

    procedual macro

---
+++ procedual/hello-world-macro/src/lib.rs
@@ -0,0 +1,19 @@
+use quote::quote;
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+
+#[proc_macro_derive(Hello)]
+pub fn hello(item: TokenStream) -> TokenStream {
+    println!("{}", item);
+    let ast = parse_macro_input!(item as DeriveInput);
+    let name = ast.ident;
+    println!("{}", name);
+    let add_hello_world = quote! {
+        impl #name {
+            fn hello_world(&self) {
+                println!("Hello, World");
+            }
+        }
+    };
+    add_hello_world.into()
+}
---
+++ procedual/src/main.rs
@@ -0,0 +1,24 @@
+// #![feature(trace_macros)]
+
+#[macro_use]
+extern crate hello_world_macro;
+
+
+#[derive(Hello)]
+struct Example;
+
+#[derive(Hello)]
+enum Pet {
+    Dog,
+    Cat,
+}
+
+fn main() {
+    //trace_macros!(true);
+    let e = Example;
+    // trace_macros!(false);
+    e.hello_world();
+
+    let d = Pet::Dog;
+    d.hello_world();
+}
